04b2699e1fe21e61146f6baa28b82dbbb17fd8b3
Mari <cys.mariana@gmail.com>
2026-01-20 15:03:17 -0300

    testes

--- Aulas Programação Web - Dimitri/js/index.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS</b> escrito com (document.getElementById)"
-alert(10 + 5)
-console.log("Oi isso é um console.log")
---
+++ Aulas Programação Web - Dimitri/js/script.js
@@ -0,0 +1,10 @@
+var nome, sobrenome, nomeCompleto
+
+nome = "Mariana";
+sobrenome = "Tosta";
+idade = 30;
+nomeCompleto = nome + " " + sobrenome;
+
+pessoa = idade + nome;
+
+document.getElementById("texto").innerHTML = pessoa;
