c92f8b3d5719e32fa78496cf42edb29a5b6c6ecf
Mari <cys.mariana@gmail.com>
2026-02-03 21:06:22 -0300

    Aulad

---
+++ Aulas Curso - Senac/ex_02.js
@@ -0,0 +1,6 @@
+let base = parseFloat(prompt("Digite a base"))
+let altura = parseFloat(prompt("Digite a altura"))
+
+let area = base * altura
+
+document.writeln(`A área é ${area}`)
---
+++ Aulas Curso - Senac/ex_06.js
@@ -0,0 +1,7 @@
+let media = parseFloat(prompt("Digite a media do aluno"))
+
+if (media >= 6) {
+    document.writeln("Aprovado")
+} else {
+    document.writeln("Reprovado")
+}
